68428994a4272f7907df921ae6af8a5f255eca0f
wildneifrank <wildneif2@gmail.com>
2023-05-24 15:47:29 -0300

    add requisitos

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fastify = require("fastify");
+
+function init() {
+  const app = fastify();
+  app.get("/", (request, reply) => reply.send({ hello: "world" }));
+  return app;
+}
+
+if (require.main === module) {
+  // called directly i.e. "node app"
+  init().listen({ port: 3000 }, (err) => {
+    if (err) console.error(err);
+    console.log("server listening on 3000");
+  });
+} else {
+  // required as a module => executed on aws lambda
+  module.exports = init;
+}
